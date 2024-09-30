824657c0c39bdf1babc9bb24c842d84204ff9b18
GonzaQuedena01 <gquedena1@gmail.com>
2024-09-30 18:35:35 -0500

    feat(Data): add method for product registration.

--- Data/DataBaseConnection.cs
+++ Data/DataBaseConnection.cs
@@ -30,7 +30,9 @@ namespace Data
             {
                 __Connection.Open();
 
-                string query = "INSERT INTO users (username, password, role) VALUES (@username, @password)";
+                string query = @"
+                    INSERT INTO users (username, password, role) 
+                        VALUES (@username, @password)";
 
                 using (SQLiteCommand cmd = new SQLiteCommand(query, __Connection))
                 {
@@ -42,6 +44,31 @@ namespace Data
             } 
         }
 
+        public void RegisterProduct(Product product)
+        {
+            using (__Connection)
+            {
+                __Connection.Open();
+
+                string query = @"
+                    INSERT INTO products (category_id, product_name, price, created_at, updated_at, created_by, updated_by, is_active) 
+                        VALUES (@category_id, @product_name, @price, @created_at, @updated_at, @created_by, @updated_by, @is_active)";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, __Connection))
+                {
+                    cmd.Parameters.AddWithValue("@category_id", product.CategoryId);
+                    cmd.Parameters.AddWithValue("@product_name", product.ProductName);
+                    cmd.Parameters.AddWithValue("@price", product.Price);
+                    cmd.Parameters.AddWithValue("@created_at", DateTime.Now); // o usa product.CreatedAt si se pasa desde la instancia
+                    cmd.Parameters.AddWithValue("@updated_at", DateTime.Now); // o usa product.UpdatedAt
+                    cmd.Parameters.AddWithValue("@created_by", product.CreatedBy);
+                    cmd.Parameters.AddWithValue("@updated_by", product.UpdatedBy);
+                    cmd.Parameters.AddWithValue("@is_active", product.IsActive ? 1 : 0);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
 
     }
 }
