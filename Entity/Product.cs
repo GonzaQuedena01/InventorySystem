743c4166952d5ef45e9b20d89fbba9a81d1f1218
GonzaQuedena01 <gquedena1@gmail.com>
2024-09-30 18:26:02 -0500

    feat(Entity): add Product entity.

---
+++ Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set;}
+        public bool IsActive { get; set; }
+    }
+}
