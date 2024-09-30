88e0d47c0f8fab170b4d978ebe31589606bfae6a
GonzaQuedena01 <gquedena1@gmail.com>
2024-09-30 16:58:32 -0500

    feat(Data): add data layer and connection to db.

---
+++ Data/DataBaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace Data
+{
+    internal class DataBaseConnection
+    {
+
+        SQLiteConnection cn = new SQLiteConnection(
+            ConfigurationManager.ConnectionStrings["sqliteconex"].ConnectionString
+        );
+    }
+}
